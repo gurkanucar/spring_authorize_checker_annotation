ae8dd7302e61ab5b80231e54e50e77e9b914d91c
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-17 01:48:45 +0300

    project initialized

---
+++ src/main/java/com/gucardev/spring_boot_authorize_checker/SpringBootAuthorizeCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.spring_boot_authorize_checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthorizeCheckerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootAuthorizeCheckerApplication.class, args);
+  }
+
+}
